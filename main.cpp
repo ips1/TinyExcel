dd2fdf219ca695b458c48634594f640c9e926391
ips1 <nando128@gmail.com>
2014-02-12 23:51:52 +0100

    New test examples

--- main.cpp
+++ main.cpp
@@ -8,46 +8,21 @@ using namespace std;
 
 int main()
 {
-    PostfixExpression expr;
 
 
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Value(5)))));
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Value(1)))));
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Value(2)))));
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Addition))));
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Value(4)))));
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Multiplication))));
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Addition))));
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Value(3)))));
-    expr.add_element(PostfixElement(std::move(std::unique_ptr<PostfixAtom>(new Subtraction))));
-
-    std::cout << expr.evaluate() << std::endl;
-
-    std::vector<char> delims = {'+', '-', '*', '/', '(', ')'};
-    std::vector<std::string> result = split_string("(aho j zz + 18x + 9*AH84 + ABV + 3.179 + 8.64 * + ++ () + ( 5 + ) ( )) + 35x + 891", delims, true);
-
-    std::cout << result.size() << std::endl;
-
-    for (auto it = result.begin(); it < result.end(); it++)
-    {
-        trim(*it, ' ');
-        std::cout << '"' << *it << '"' << std::endl;
-    }
-
-    std::vector<CellReference> dependencies;
     Table t;
-    //PostfixExpression expr2 = parse_infix("5+(3   * 8 -9) / 125 * 8 + (35) - (35 * 65 + (42 - 8))", t, dependencies);
-    PostfixExpression expr2 = parse_infix("(6*1 + 6*1 + 6*1 + 2*1 + 5*1 + 3*1 + 6*1 + 6*3) / 40", t, dependencies);
-    std::cout << expr2.evaluate() << std::endl;
 
     t.set_cell(CellReference(1,1),"5");
     t.set_cell(CellReference(2,1),"1");
-    t.set_cell(CellReference(1,2),"= (B2) - ( (A1) - (4 * A2 + 3) )");
-    Cell c("= (A1) - (4 * A2 + 3)", t);
-//    std::cout << d.get_error() << std::endl;
+    t.set_cell(CellReference(1,2),"= (B2) - ( (A1) - (4 * A2 + 3 *A1-1) )");
+    t.set_cell(CellReference(3,3),"=(6*1 + 6*1 + 6*1 + 2*1 + 5*1 + 3*1 + 6*1 + 6*3) / 40");
     std::cout << "Done" << std::endl;
 
     std::cout << t.evaluate_cell(CellReference(1,2)) << std::endl;
+    std::cout << t.evaluate_cell(CellReference(3,3)) << std::endl;
+    std::cout << t.evaluate_cell(CellReference(80,159)) << std::endl;
+
+
 
 
     return 0;
